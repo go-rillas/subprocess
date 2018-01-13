08990c84fcdded65aa190f011cba3835b896ca54
Chris Simpkins <git.simpkins@gmail.com>
2018-01-12 23:56:38 -0500

    [subprocess.go] implementation of Run function

--- subprocess.go
+++ subprocess.go
@@ -1,2 +1,50 @@
 package subprocess
 
+import (
+	"bytes"
+	"os/exec"
+	"syscall"
+)
+
+type Response struct {
+	StdOut   string
+	StdErr   string
+	ExitCode int
+}
+
+func Run(executable string, args ...string) Response {
+	var res Response
+	var outbuf, errbuf bytes.Buffer
+
+	// define the system executable call
+	cmd := exec.Command(executable, args...)
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+	// execute the system command
+	err := cmd.Run()
+	// define the returned object fields with the data returned
+	res.StdOut = outbuf.String()
+	res.StdErr = errbuf.String()
+	
+	if err != nil {
+		// fail, non-zero exit status conditions
+		if exitError, ok := err.(*exec.ExitError); ok {
+			res.ExitCode = exitError.Sys().(syscall.WaitStatus).ExitStatus()
+		} else {
+			// fails that do not define an exec.ExitError (e.g. unable to identify executable on system PATH)
+			res.ExitCode = 1 // assign a default non-zero fail code value of 1
+			if res.StdErr == "" {
+				res.StdErr = err.Error() // return the error raised in standard error stream formatted as a string
+			}
+		}
+	} else {
+		// success, zero exit status condition
+		res.ExitCode = cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+
+	return res
+}
+
+func Pipe() {
+	// TODO
+}
--- subprocess_test.go
+++ subprocess_test.go
@@ -1,1 +1,45 @@
 package subprocess
+
+import (
+	"testing"
+)
+
+func TestRunValidCommand(t *testing.T) {
+	response := Run("git", "--help")
+	if response.ExitCode != 0 {
+		t.Errorf("[FAIL] Expected 'git --help' to return 0 exit status code and instead it returned %d", response.ExitCode)
+	}
+	if len(response.StdErr) > 0 {
+		t.Errorf("[FAIL] Expected 'git --help' to return no standard error output and instead it returned %v", response.StdErr)
+	}
+	if len(response.StdOut) == 0 {
+		t.Errorf("[FAIL] Expected 'git --help' to return standard output but instead it was empty.")
+	}
+}
+
+
+func TestRunInValidCommandBadArgument(t *testing.T) {
+	response := Run("git", "--bogus")
+	if response.ExitCode == 0 {
+		t.Errorf("[FAIL] Expected invalid argument to executable to return non-0 exit status code and instead it returned %d", response.ExitCode)
+	}
+	if len(response.StdErr) == 0 {
+		t.Errorf("[FAIL] Expected invalid argument to executable to return standard error output and instead it returned an empty string")
+	}
+	if len(response.StdOut) > 0 {
+		t.Errorf("[FAIL] Expected invalid argument to return no standard output but instead it returned %s.", response.StdOut)
+	}
+}
+
+func TestRunInvalidCommandMissingExecutable(t *testing.T) {
+	response := Run("bogus", "--help")
+	if response.ExitCode == 0 {
+		t.Errorf("[FAIL] Expected invalid command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+	}
+	if len(response.StdErr) == 0 {
+		t.Errorf("[FAIL] Expected invalid command to return standard error output and instead it returned an empty string")
+	}
+	if len(response.StdOut) > 0 {
+		t.Errorf("[FAIL] Expected invalid command to return no standard output but instead it returned %s.", response.StdOut)
+	}
+}
