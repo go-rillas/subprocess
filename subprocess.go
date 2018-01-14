48e15551dc843edfed4f9cb1385b071c869c42af
Chris Simpkins <git.simpkins@gmail.com>
2018-01-13 22:09:17 -0500

    [subprocess.go] added RunShell function to support system executable calls from shells

--- subprocess.go
+++ subprocess.go
@@ -4,7 +4,9 @@ package subprocess
 import (
 	"bytes"
 	"os/exec"
+	"runtime"
 	"syscall"
+	"strings"
 )
 
 // Response is a struct that is returned from the public functions in the subprocess package.  It contains the following
@@ -18,6 +20,21 @@ type Response struct {
 	StdErr   string
 	ExitCode int
 }
+
+/*    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┃                          ______      _     _ _                               ┃
+ *    ┃                          | ___ \    | |   | (_)                              ┃
+ *    ┃                          | |_/ /   _| |__ | |_  ___                          ┃
+ *    ┃                          |  __/ | | | '_ \| | |/ __|                         ┃
+ *    ┃                          | |  | |_| | |_) | | | (__                          ┃
+ *    ┃                          \_|   \__,_|_.__/|_|_|\___|                         ┃
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ */
 
 // Run is a public function that executes a system command and returns the standard output stream,
 // standard error stream, and exit status code data in a returned subprocess.Response struct.
@@ -35,6 +52,7 @@ type Response struct {
 //         fmt.Printf("%d\n", response.ExitCode)
 //     }
 func Run(executable string, args ...string) Response {
+	// define function variables
 	var res Response
 	var outbuf, errbuf bytes.Buffer
 
@@ -47,21 +65,56 @@ func Run(executable string, args ...string) Response {
 	// define the returned object fields with the data returned
 	res.StdOut = outbuf.String()
 	res.StdErr = errbuf.String()
-
 	if err != nil {
-		// fail, non-zero exit status conditions
-		if exitError, ok := err.(*exec.ExitError); ok {
-			res.ExitCode = exitError.Sys().(syscall.WaitStatus).ExitStatus()
-		} else {
-			// fails that do not define an exec.ExitError (e.g. unable to identify executable on system PATH)
-			res.ExitCode = 1 // assign a default non-zero fail code value of 1
-			if res.StdErr == "" {
-				res.StdErr = err.Error() // return the error raised in standard error stream formatted as a string
-			}
-		}
+		res.ExitCode = getErrorExitCode(err)
 	} else {
-		// success, zero exit status condition
 		res.ExitCode = cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+	if res.StdErr == "" && res.ExitCode != 0 {
+		res.StdErr = err.Error() // return the error raised in standard error stream formatted as a string
+	}
+
+	return res
+}
+
+func RunShell(shell string, shellflag string, command ...string) Response {
+	// define the default shell by platform
+	if shell == "" {
+		if runtime.GOOS == "windows" {
+			shell = `cmd`       // defined as "cmd \C" for Windows
+		} else {
+			shell = `/bin/sh`   // defined as "/bin/sh -c" for *nix (including macOS)
+		}
+	}
+	// define the default shell flag for execution of system executables
+	if shellflag == "" {
+		if runtime.GOOS == "windows" {
+			shellflag = `\C`
+		} else {
+			shellflag = `-c`
+		}
+	}
+	// define function variables
+	var res Response
+	var outbuf, errbuf bytes.Buffer
+
+	// define the system executable call
+	shellExecString := strings.Join(command, " ")
+	cmd := exec.Command(shell, shellflag, shellExecString)
+	cmd.Stdout = &outbuf
+	cmd.Stderr = &errbuf
+	// execute the system command
+	err := cmd.Run()
+	// define the returned object fields with the data returned
+	res.StdOut = outbuf.String()
+	res.StdErr = errbuf.String()
+	if err != nil {
+		res.ExitCode = getErrorExitCode(err)
+	} else {
+		res.ExitCode = cmd.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+	if res.StdErr == "" && res.ExitCode != 0 {
+		res.StdErr = err.Error() // return the error raised in standard error stream formatted as a string
 	}
 
 	return res
@@ -70,3 +123,29 @@ func Run(executable string, args ...string) Response {
 //func Pipe() {
 //	// TODO
 //}
+
+/*    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┃                       ______     _            _                              ┃
+ *    ┃                       | ___ \   (_)          | |                             ┃
+ *    ┃                       | |_/ / __ ___   ____ _| |_ ___                        ┃
+ *    ┃                       |  __/ '__| \ \ / / _` | __/ _ \                       ┃
+ *    ┃                       | |  | |  | |\ V / (_| | ||  __/                       ┃
+ *    ┃                       \_|  |_|  |_| \_/ \__,_|\__\___|                       ┃
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┃                                                                              ┃
+ *    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ */
+
+// getErrorExitCode returns an integer value representing the exit code status for non-zero exit code responses from
+// the executable called in the public functions in the subprocess package
+func getErrorExitCode(err error) int {
+	// fail, non-zero exit status conditions
+	if exitError, ok := err.(*exec.ExitError); ok {
+		return exitError.Sys().(syscall.WaitStatus).ExitStatus()
+	}
+	// fails that do not define an exec.ExitError (e.g. unable to identify executable on system PATH)
+	return 1 // assign a default non-zero fail code value of 1
+}
--- subprocess_test.go
+++ subprocess_test.go
@@ -2,18 +2,21 @@ package subprocess
 
 import (
 	"testing"
+	"runtime"
 )
+
+// Run() function tests
 
 func TestRunValidCommand(t *testing.T) {
 	response := Run("git", "--help")
 	if response.ExitCode != 0 {
-		t.Errorf("[FAIL] Expected 'git --help' to return 0 exit status code and instead it returned %d", response.ExitCode)
+		t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
 	}
 	if len(response.StdErr) > 0 {
-		t.Errorf("[FAIL] Expected 'git --help' to return no standard error output and instead it returned %v", response.StdErr)
+		t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
 	}
 	if len(response.StdOut) == 0 {
-		t.Errorf("[FAIL] Expected 'git --help' to return standard output but instead it was empty.")
+		t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
 	}
 }
 
@@ -40,5 +43,343 @@ func TestRunInvalidCommandMissingExecutable(t *testing.T) {
 	}
 	if len(response.StdOut) > 0 {
 		t.Errorf("[FAIL] Expected invalid command to return no standard output but instead it returned %s.", response.StdOut)
+	}
+}
+
+// RunShell() function tests - *nix platform
+
+func TestRunShellUnixValidDefaultShellCommandOneString(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "", "git --help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidDefaultShellCommandOneStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "-c", "git --help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidDefaultShellCommandTwoStrings(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "", "git", "--help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidDefaultShellCommandTwoStringsWithShellFlag(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "-c", "git", "--help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+
+func TestRunShellUnixValidBashShellCommandOneString(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("/usr/local/bin/bash", "", "git --help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidBashShellCommandOneStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("/usr/local/bin/bash", "-c", "git --help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidBashShellCommandTwoString(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("/usr/local/bin/bash", "", "git", "--help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixValidBashShellCommandTwoStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("/usr/local/bin/bash", "-c", "git", "--help")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellUnixInvalidShell(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("/usr/local/bin/bogusshell", "", "git", "--help")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
+	}
+}
+
+func TestRunShellUnixInvalidExecutable(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "", "completelybogus", "--help")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
+	}
+}
+
+func TestRunShellUnixInvalidExecutableArgument(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		response := RunShell("", "", "git", "--bogus")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
+	}
+}
+
+////////////////////////////////////////////////
+//  RunShell() function tests - Windows platform
+////////////////////////////////////////////////
+
+func TestRunShellWindowsValidDefaultShellCommandOneString(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", "", "dir /ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidDefaultShellCommandOneStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", `\C`, "dir /ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidDefaultShellCommandTwoStrings(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", "", "dir", "/ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidDefaultShellCommandTwoStringsWithShellFlag(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", `\C`, "dir", "/ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+
+func TestRunShellWindowsValidMockShellCommandOneString(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("cmd", "", "dir /ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidMockShellCommandOneStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("cmd", `\C`, "dir /ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidMockShellCommandTwoString(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("cmd", "", "dir", "/ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsValidMockShellCommandTwoStringWithShellFlag(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("cmd", `\C`, "dir", "/ad")
+		if response.ExitCode != 0 {
+			t.Errorf("[FAIL] Expected command to return 0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard error output and instead it returned %v", response.StdErr)
+		}
+		if len(response.StdOut) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard output but instead it was empty.")
+		}
+	}
+}
+
+func TestRunShellWindowsInvalidShell(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("totallybogusshell", "", "dir", "/ad")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
+	}
+}
+
+func TestRunShellWindowsInvalidExecutable(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", "", "completelybogus", "/ad")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
+	}
+}
+
+func TestRunShellWindowsInvalidExecutableArgument(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		response := RunShell("", "", "dir", "/zzz")
+		if response.ExitCode == 0 {
+			t.Errorf("[FAIL] Expected command to return non-0 exit status code and instead it returned %d", response.ExitCode)
+		}
+		if len(response.StdErr) == 0 {
+			t.Errorf("[FAIL] Expected command to return standard error output and instead it returned empty string")
+		}
+		if len(response.StdOut) > 0 {
+			t.Errorf("[FAIL] Expected command to return no standard output but instead it returned %s.", response.StdOut)
+		}
 	}
 }
